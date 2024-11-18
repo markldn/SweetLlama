7f6b587973fd9c72f2c50d294f0260f1ddeddb25
Drogan Cintam <drogancintam@gmail.com>
2024-11-18 22:25:03 +0700

    Fix revision of llama.cpp
    to avoid unstable commits.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["SweetLlama"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ggerganov/llama.cpp", branch: "master"),
+        .package(url: "https://github.com/ggerganov/llama.cpp", revision: "9b75f03"), // b4122
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
