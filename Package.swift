0e76829ff2d41c79d4ce62d1de8364bbc2327c82
markldn <markldn@gmail.com>
2025-06-23 21:31:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -11,17 +9,14 @@ let package = Package(
         .iOS(.v15),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SweetLlama",
             targets: ["SweetLlama"]),
     ],
     dependencies: [
-       .package(url: "https://github.com/markldn/llama.cpp.git", branch: "master"),
+        .package(url: "https://github.com/markldn/llama.cpp.git", branch: "master"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SweetLlama",
             dependencies: [
